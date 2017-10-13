480ef338055dc30c404182d5234fc240ba4625f7
Daniel Fernandes <danielfernandes@Daniel-Fernandess-MacBook-Pro.local>
2017-10-13 19:39:14 -0400

    shola changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React, { Component } from 'react';
 import './App.css';
 import PhotoFeed from './PhotoFeed.js'
 
+const TUMBLR_API_KEY = '4c9zOZau7XjtOpYe4vYoGVkKwZwFpSCwNLhs8VsWYqIpE90Yvv';
+const TUMBLR_BLOG_ID = 'missing-time.tumblr.com';
+
 class App extends Component {
   render() {
     return (
@@ -10,7 +13,10 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title"></h1>
         </header>
-        <PhotoFeed>
+        <PhotoFeed
+          apiKey={TUMBLR_API_KEY}
+          blogId={TUMBLR_BLOG_ID}
+        >
           {this.props.children}
         </PhotoFeed>
         
@@ -20,3 +26,4 @@ class App extends Component {
 }
 
 export default App;
+
--- src/PhotoFeed.js
+++ src/PhotoFeed.js
@@ -1,47 +1,70 @@
-
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import tumblr from 'tumblr.js';
 import './App.css';
-
- 
-var tumblr = require('tumblr.js');
-
-var client = tumblr.createClient({ consumer_key: '4c9zOZau7XjtOpYe4vYoGVkKwZwFpSCwNLhs8VsWYqIpE90Yvv' });
-
-
-client.blogPosts('missing-time.tumblr.com', posts);
-
-function posts(err, data) {
-
-        data.posts;
-        console.log('here is the info', data.posts)
-    };
-
-
 
 class PhotoFeed extends Component {
 
-constructor(props) {
-        super(props); 
-         this.state = {
-            photos: []
-        };
-      }
-    
+  constructor(props) {
+    super(props);
 
+    // Tumblr client.
+    this.tumblr = tumblr.createClient({
+      consumer_key: props.apiKey
+    });
 
-render() {
-
-
-return (
-   
-        <div >
-           <p>photofeed will be here</p>
-
-        </div>
-        
-    );
+    this.state = {
+      photos: []
+    }
   
+    // Get the first 20 photos when the component is created.
+    this.fetchPhotos({limit: 20, offset: 0})
+      .then(photos => this.setState({photos}))
+      .catch(console.error);
+  }
+
+  /**
+   * Fetch blog's photos using the Tumblr API.
+   * `options` can contain any of the values listed here:
+   *    https://www.tumblr.com/docs/en/api/v2#posts
+   * 
+   * @param {Object} [options={}] 
+   * @returns {Promise}
+   * @memberof PhotoFeed
+   */
+  fetchPhotos(options = {}) {
+    return new Promise((resolve, reject) => {
+      const {blogId} = this.props;
+      options.type = 'photo';
+      this.tumblr.blogPosts(blogId, options, (err, {posts}) => {
+        if (err) {
+          return reject(err);
+        } else {
+          // Transform `posts` array into an array of photo URLs.
+          const photos = posts.map(post => post.photos[0].original_size.url);
+          return resolve(photos);
+        }
+      });
+    });
+  }
+
+
+  render() {
+    const {photos} = this.state;
+    return (
+      <ul className='photo-feed'>
+        {photos.map(photo => (
+          <li><img src={photo} width="500" /></li>
+        ))}
+      </ul>
+    );
+
   }
 }
+
+PhotoFeed.propTypes = {
+  apiKey: PropTypes.string.isRequired,
+  blogId: PropTypes.string.isRequired
+};
 
 export default PhotoFeed;
